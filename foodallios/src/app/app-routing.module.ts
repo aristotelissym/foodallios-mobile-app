a0923e309de749c72c37185d367ced9ee40d2094
foodallios <foodallios@gmail.com>
2022-09-19 21:40:00 +0300

    build welcome page and login page with links and some style

--- foodallios/src/app/app-routing.module.ts
+++ foodallios/src/app/app-routing.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { AutoLoginGuard } from './guards/auto-login.guard';
+import { IntroGuard } from './guards/intro.guard';
 
 const routes: Routes = [
   {
     path: 'tabs',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule),
+    canLoad: [AuthGuard]
   },
   {
     path: '',
     loadChildren: () => import('./pages/welcome-page/welcome-page.module').then( m => m.WelcomePagePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule),
+    canLoad: [IntroGuard, AutoLoginGuard]
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () => import('./pages/forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
   }
 ];
 @NgModule({
--- foodallios/src/app/pages/welcome-page.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { WelcomePagePipe } from './welcome-page.pipe';
-
-describe('WelcomePagePipe', () => {
-  it('create an instance', () => {
-    const pipe = new WelcomePagePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
