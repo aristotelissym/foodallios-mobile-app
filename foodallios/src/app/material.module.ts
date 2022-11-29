d2bd0624d6576fff2eeece5aa32b336243c9a01a
foodallios <foodallios@gmail.com>
2022-11-29 16:22:36 +0200

    sign up and login view

--- foodallios/src/app/material.module.ts
+++ foodallios/src/app/material.module.ts
@@ -14,6 +14,7 @@ import { MatListModule } from "@angular/material/list";
 import { MatGridListModule } from "@angular/material/grid-list";
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatBadgeModule} from '@angular/material/badge';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 
 @NgModule({
@@ -33,7 +34,8 @@ import {MatBadgeModule} from '@angular/material/badge';
     MatListModule,
     MatGridListModule,
     MatToolbarModule,
-    MatBadgeModule
+    MatBadgeModule,
+    MatSidenavModule
    
   ]
 })
--- foodallios/src/app/pages/signup/signup.module.ts
+++ foodallios/src/app/pages/signup/signup.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { SignupPageRoutingModule } from './signup-routing.module';
 
 import { SignupPage } from './signup.page';
+import { MaterialModule } from 'src/app/material.module';
 
 @NgModule({
   imports: [
@@ -14,7 +15,8 @@ import { SignupPage } from './signup.page';
     FormsModule,
     IonicModule,
     SignupPageRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MaterialModule
   ],
   declarations: [SignupPage]
 })
